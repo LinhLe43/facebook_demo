ecc32f1def7bec95abe642e3f8e21a9e7487b8a8
Dank21 <dandovan145@gmail.com>
2023-12-01 20:45:18 +0700

    dan-12/1

---
+++ src/main/java/com/example/facebook_servlet_demo/service/CategoryService.java
@@ -0,0 +1,28 @@
+package com.example.facebook_servlet_demo.service;
+
+import com.example.facebook_servlet_demo.model.Category;
+import com.example.facebook_servlet_demo.service.IService.ICategoryService;
+
+import java.util.List;
+
+public class CategoryService implements ICategoryService<Category> {
+    @Override
+    public void add(Category category) {
+
+    }
+
+    @Override
+    public void edit(int id, Category category) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Category> findAll() {
+        return null;
+    }
+}
