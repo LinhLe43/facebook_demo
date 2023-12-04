a14a053516e3e36805a2c761910d089d1d1351f6
Dank21 <dandovan145@gmail.com>
2023-12-04 18:46:18 +0700

    dan-12/4

--- src/main/java/com/example/facebook_servlet_demo/service/IService/IUserService.java
+++ src/main/java/com/example/facebook_servlet_demo/service/IService/IUserService.java
@@ -2,9 +2,9 @@ package com.example.facebook_servlet_demo.service.IService;
 
 import java.util.List;
 
-public interface ICategoryService<C> {
+public interface IUserService<C> {
     void add(C c);
     void edit(int id, C c);
     void delete(int id);
-    List<C> findAllCategory();
+    List<C> findAllUser();
 }
---
+++ src/main/java/com/example/facebook_servlet_demo/service/SituationService.java
@@ -0,0 +1,28 @@
+package com.example.facebook_servlet_demo.service;
+
+import com.example.facebook_servlet_demo.model.Situation;
+import com.example.facebook_servlet_demo.service.IService.ISituationService;
+
+import java.util.List;
+
+public class SituationService implements ISituationService<Situation> {
+    @Override
+    public void add(Situation situation) {
+
+    }
+
+    @Override
+    public void edit(int id, Situation situation) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Situation> findAllSituation() {
+        return null;
+    }
+}
