c764e5a04b56346d695bf506b3fd4e19fe9a4789
Lê Dũng Linh <linhld@hocmai.vn>
2023-12-04 20:56:03 +0700

    Linh update sql & postService 4.12.2023

--- database/store.sql
+++ database/store.sql
@@ -4,49 +4,80 @@ drop database blog;
 
 create table user
 (
-    id int not null primary key auto_increment,
-    name varchar(60) not null,
+    id              int         not null primary key auto_increment,
+    name            varchar(60) not null,
     profile_picture text,
-    sex varchar(10),
-    dob varchar(30),
-    address text
+    sex             varchar(10),
+    dob             varchar(30),
+    address         text
 );
 
 create table post
 (
-    id int not null primary key auto_increment,
-    content text,
-    image text,
-    time varchar(30),
+    id          int not null primary key auto_increment,
+    content     text,
+    image       text,
+    time        varchar(30),
     idSituation int,
-    foreign key (idSituation) references situation(id),
-    idCategory int,
-    foreign key (idCategory) references category(id),
-    idUser int,
-    foreign key (idUser) references user(id)
+    foreign key (idSituation) references situation (id),
+    idCategory  int,
+    foreign key (idCategory) references category (id),
+    idUser      int,
+    foreign key (idUser) references user (id)
 );
 
 create table situation
 (
-    id int not null primary key auto_increment,
+    id   int not null primary key auto_increment,
     name varchar(20)
 );
 
 create table category
 (
-    id int not null primary key auto_increment,
+    id   int not null primary key auto_increment,
     name varchar(20)
 );
 
 create table comment
 (
-    id int not null primary key auto_increment,
-    time varchar(20),
+    id      int not null primary key auto_increment,
+    time    varchar(20),
     content text,
-    image text,
-    idPost int,
-    foreign key (idPost) references post(id),
-    idUser int,
-    foreign key (idUser) references user(id)
+    image   text,
+    idPost  int,
+    foreign key (idPost) references post (id),
+    idUser  int,
+    foreign key (idUser) references user (id)
 );
+
+-- Find All Comment
+select c.*, u.name
+from comment c
+         join user u on u.id = c.idUser;
+
+-- Find All Post
+select p.*, c.name as categoryName, s.name as situationName, u.name as userName
+from post p
+         join category c on c.id = p.idCategory
+         join situation s on p.idSituation = s.id
+         join user u on p.idUser = u.id;
+
+-- Add Post
+insert into post(content, image, time, idSituation, idCategory, idUser)
+values (?, ?, ?, ?, ?, ?);
+
+-- Update Post
+update post
+set content     = ?,
+    image       = ?,
+    time        = ?,
+    idSituation = ?,
+    idCategory  = ?,
+    idUser      = ?
+where id = ?;
+
+-- Delete Post
+delete
+from post
+where id = ?;
 
--- src/main/java/com/example/facebook_servlet_demo/service/PostService.java
+++ src/main/java/com/example/facebook_servlet_demo/service/PostService.java
@@ -3,27 +3,75 @@ package com.example.facebook_servlet_demo.service;
 import com.example.facebook_servlet_demo.model.Post;
 import com.example.facebook_servlet_demo.service.IService.IPostService;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PostService implements IPostService<Post> {
+    Connection connection = ConnectToMySQL.getConnection();
+
     @Override
     public void add(Post post) {
-
+        String sql = "insert into post(content, image, time, idSituation, idCategory, idUser)\n" +
+                "values (?, ?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, post.getContent());
+            preparedStatement.setString(2, post.getImage());
+            preparedStatement.setString(3, post.getTime());
+            preparedStatement.setInt(4, post.getSituation().getId());
+            preparedStatement.setInt(5, post.getCategory().getId());
+            preparedStatement.setInt(6, post.getUser().getId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void edit(int id, Post post) {
-
+        String sql = "update post\n" +
+                "set content     = ?,\n" +
+                "    image       = ?,\n" +
+                "    time        = ?,\n" +
+                "    idSituation = ?,\n" +
+                "    idCategory  = ?,\n" +
+                "    idUser      = ?\n" +
+                "where id = ?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, post.getContent());
+            preparedStatement.setString(2, post.getImage());
+            preparedStatement.setString(3, post.getTime());
+            preparedStatement.setInt(4, post.getSituation().getId());
+            preparedStatement.setInt(5, post.getCategory().getId());
+            preparedStatement.setInt(6, post.getUser().getId());
+            preparedStatement.setInt(7, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void delete(int id) {
-
+        String sql = "delete from post where id = ?;";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public List<Post> findAll() {
-        return null;
+        List<Post> posts = new ArrayList<>();
+
+        return posts;
     }
 
 }
