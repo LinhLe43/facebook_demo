ad6a02e5b0234d49c8f8b991d21f7a7417e8a525
GinzaTech <thanhloc150204@gmail.com>
2023-12-06 19:05:59 +0700

    hello

---
+++ src/main/java/com/example/facebook_servlet_demo/home.java
@@ -0,0 +1,11 @@
+package com.example.facebook_servlet_demo;
+
+import java.util.Arrays;
+
+public class home {
+    public static void main(String[] args) {
+        int[][] daChieu = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        System.out.println(daChieu);
+    }
+}
+
